c406d87ccbadb4f84514c5e0bc41672e2eb756aa
Salwerkas <salwerkas@gmail.com>
2018-12-05 15:22:53 +0100

    Lab 5 zrobione

--- Lab5/baza/connectiontoDB.php
+++ Lab5/baza/connectiontoDB.php
@@ -9,7 +9,7 @@ $age = $_POST['Wiek'];
 $query  = "INSERT INTO test (Imie,Nazwisko,Wiek)
             VALUES ('$name','$surname',$age)";
     if ($link->query($query) === TRUE) {
-        echo "New record created successfully";
+        echo nl2br("New record created successfully \n");
     } else {
         echo "Error: " . $query . "<br>" . $link->error;
     }
@@ -26,11 +26,11 @@ $result = mysqli_query($link, $query)
     or die("Error");
 while ($row = mysqli_fetch_array($result)) {
     echo
-  "<b><TR>Wiek:&nbsp&nbsp". $row["Wiek"]."</b><br/>".
+    nl2br("\n<b><TR>Wiek:&nbsp&nbsp". $row["Wiek"]."</b><br/>".
   "<b><TR>name:&nbsp&nbsp". $row["Imie"]."</b><br/>".
     "<b><TR>surname:&nbsp&nbsp". $row["Nazwisko"]."</b><br/>".
     "<b><TR>Id:&nbsp&nbsp". $row["ID"]."</b><br/>".
-    "</TR><TR></TR>\n<br/>";
+    "</TR><TR></TR>\n<br/>");
     }
 mysqli_free_result($result);
 mysqli_close($link);
--- Lab5/baza/connectiontoDB2.php
+++ Lab5/baza/connectiontoDB2.php
@@ -3,18 +3,15 @@ $link = mysqli_connect("localhost", "root", "")
     or die("Could not connect");
 mysqli_select_db($link, "testowa")
     or die("Could not select database");
-mysqli_query($link, "SET NAMES utf8");
-$query  = "SELECT * FROM id";
-$result = mysqli_query($link, $query)
-    or die("Error");
-while ($row = mysqli_fetch_array($result)) {
-    echo
-  "<b><TR>Wiek:&nbsp&nbsp". $row["Wiek"]."</b><br/>".
-  "<b><TR>name:&nbsp&nbsp". $row["Imie"]."</b><br/>".
-    "<b><TR>surname:&nbsp&nbsp". $row["Nazwisko"]."</b><br/>".
-    "<b><TR>Id:&nbsp&nbsp". $row["ID"]."</b><br/>".
-    "</TR><TR></TR>\n<br/>";
-    }
-mysqli_free_result($result);
+    $name = $_POST['Imie'];
+$surname = $_POST['Nazwisko'];
+$age = $_POST['Wiek'];
+$id = $_POST['ID'];
+$sql = "UPDATE test SET Imie='$name', Nazwisko='$surname', Wiek='$age' WHERE id='$id'";
+if ($link->query($sql) === TRUE) {
+    echo "Record updated successfully \n";
+} else {
+    echo "Error updating record: " . $sql . "<br>" . $link->error;
+}
 mysqli_close($link);
 ?>
